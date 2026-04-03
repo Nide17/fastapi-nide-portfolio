79705553f8c2ecc34ef858f368593731a96b977d
Parmenide <nidedrogba@gmail.com>
2026-04-03 14:22:52 +0200

    Projects: SQLAlchemy models & Pydantic schemas

---
+++ app/models/project.py
@@ -0,0 +1,16 @@
+# DB Tables
+from sqlalchemy import Column, Integer, String, ARRAY, DateTime
+from app.db.session import Base
+
+class Project(Base):
+    __tablename__ = "projects"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True, nullable=False, unique=True)
+    description = Column(String)
+    image = Column(String)
+    github = Column(String)
+    live_at = Column(String)
+    technologies = Column(ARRAY(String)) # Array of strings like ["FastAPI", "React"]
+    created_at = Column(DateTime)
+    
---
+++ app/schemas/project.py
@@ -0,0 +1,17 @@
+# Pydantic Schemas or models that validate and serialize incoming and outgoing data for the Project
+from pydantic import BaseModel, ConfigDict
+from typing import List, Optional
+
+class ProjectBase(BaseModel):
+    title: str
+    description: Optional[str] = None
+    image: Optional[str] = None
+    github: Optional[str] = None
+    live_at: Optional[str] = None
+    technologies: Optional[List[str]] = None
+
+class ProjectOut(ProjectBase):
+    id: int
+    created_at: Optional[str] = None
+
+    model_config = ConfigDict(from_attributes=True) # Allows Pydantic to read SQLAlchemy objects
